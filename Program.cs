92c8afc74df2ebb8ee9625ba18d3e22ef4991c8d
Manoj Bastola <manojbastola676@gmail.com>
2025-11-29 13:07:18 +0545

    Initial Commit - Setup config

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// to make file organizer
+// I need to categorize the type of file that i want to organize
+// And need to make those folder with category
+// After downloading the files, i have to move these file to the respective oranized folder.
+// Need to run the console using windows taks scheduler and cron job
+
+
+
+
+
+
